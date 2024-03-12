0c2c7bd8585b4336e3a078e19ecdded4928ab5ee
APlonks <antoine.planque@gmail.com>
2024-03-12 17:35:01 +0100

    Working on pop up for API from front

--- frontend/src/components/global/Navbar.vue
+++ frontend/src/components/global/Navbar.vue
@@ -3,6 +3,8 @@ import { ref } from "vue";
 import { RouterLink } from 'vue-router'
 import Image from 'primevue/image';
 import Button from 'primevue/button';
+import Dialog from 'primevue/dialog'
+import InputText from 'primevue/inputtext'
 
 // Path for light and dark theme
 const darkThemePath = '/themes/aura-dark-blue/theme.css';
@@ -10,6 +12,8 @@ const lightThemePath = '/themes/aura-light-blue/theme.css';
 
 // Ref to follow the current theme
 const currentThemePath = ref(lightThemePath);
+
+const visible = ref<boolean>(false)
 
 function toggleTheme() {
     // To change theme
@@ -27,15 +31,35 @@ function toggleTheme() {
 
 <template>
     <div class="navbar">
-        <div>
-            <h3 >Ether-faucet</h3>
+        <div class="pop_up_container">
+            <Button class="pop_up_addr" @click="visible = true" label="Configuration"/>
+            <Dialog v-model:visible="visible" modal header="Edit configuration" :style="{ width: '30rem' }">
+                <span class="p-text-secondary block mb-5">...</span>
+                <div class="flex align-items-center space-around gap-3 mb-3">
+                    <label for="username" class="font-semibold w-6rem">Backend API</label>
+                    <InputText id="username" class="flex-auto" autocomplete="off" />
+                </div>
+                <div class="flex align-items-center gap-3 mb-5">
+                    <label for="email" class="font-semibold w-6rem">Node HTTP endpoint</label>
+                    <InputText id="email" class="flex-auto" autocomplete="off" />
+                </div>
+                <div class="flex align-items-center gap-3 mb-5">
+                    <label for="email" class="font-semibold w-6rem">Node WS endpoint</label>
+                    <InputText id="email" class="flex-auto" autocomplete="off" />
+                </div>
+                <div class="flex justify-content-end gap-2">
+                    <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+                    <Button type="button" label="Save" @click="visible = false"></Button>
+                </div>
+            </Dialog>
         </div>
         <div class="ethereum_logo">
+            <h3 class="application_title">Ether-faucet</h3>
             <RouterLink class="home_link" to="/">
                 <Image src="/Ethereum_logo.png" alt="Image" width="40" />
             </RouterLink>
-            </div>
-        <div>
+        </div>
+        <div class="dark_mode_button">
             <Button @click="toggleTheme" label="Dark Mode" />
         </div>
     </div>
@@ -49,7 +73,33 @@ function toggleTheme() {
     margin: 1%;
 }
 
+.pop_up_container{
+    width: 10rem;
+}
+
+.pop_up_addr{
+    margin-top: 0rem;
+}
+
+.ethereum_logo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.application_title{
+    margin-top: 0rem;
+}
+
 .home_link{
     text-decoration:none;
 }
+
+.dark_mode_button{
+    width: 10rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
 </style>
